c066246224aea7206d64fde0345a593972791f8a
Mostafa <mostafa.nassar829@gmail.com>
2026-07-10 15:03:12 +0300

    Bug Fix

--- server/Controller/authController.js
+++ server/Controller/authController.js
@@ -74,7 +74,7 @@ export const login= async (req, res) => {
             maxAge: 7 * 24 * 60 *60 * 1000
         });
 
-        res.status(200).json({message: "Login succesful", token})
+        res.status(200).json({message: "Login succesful"})
 
     } catch (error) {
         res.status(500).json({ errorMessage: error.message });
