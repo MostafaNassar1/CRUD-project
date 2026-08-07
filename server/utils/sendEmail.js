47afdc7cccf436a2319993633b8264adf77694c0
Mostafa <mostafa.nassar829@gmail.com>
2026-08-07 13:34:27 +0300

    Switch to Resend API

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,4 +1,39 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+// send email utility function
+const sendEmail = async (to, subject, html) => {
+    try {
+        const { error } = await resend.emails.send({
+            from: "CRUD App <onboarding@resend.dev>",
+            to,
+            subject,
+            html
+        });
+
+        if (error) {
+            console.error(`❌ Email error:`, error);
+            return;
+        }
+
+        console.log(`✅ Email sent to ${to}`);
+    } catch (error) {
+        console.error(`❌ Email error: ${error.message}`);
+    }
+};
+
+export default sendEmail;
+
+
+
+
+
+
+
+
+//USING NODEMAILER/SMTP
+/*import nodemailer from "nodemailer";
 
 // create transporter — connection to Gmail
 const transporter = nodemailer.createTransport({
@@ -25,4 +60,4 @@ const sendEmail = async (to, subject, html) => {
     }
 };
 
-export default sendEmail;
+export default sendEmail;*/
